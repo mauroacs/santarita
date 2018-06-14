d1e640707f81c850e36d3267befd4018a8d263b0
Mauro Siqueira <mauro@mauroacs.com.br>
2018-06-14 19:08:13 -0300

    Repositorio de Aula da Fauldade Santa Rita. Backup pessoal.

---
+++ estudo/agenda/core.php
@@ -0,0 +1,23 @@
+<?php
+	$host = "localhost";
+	$user = "mauro";
+	$pass = "123";
+	$base = "agenda";
+	
+	class pessoa {
+	private $codigo, $nome, $dt_nasc, $nome_pai, $nome_mae,
+		$doc_rg, $doc_cpf;
+	public function insere($par_codigo,$par_nome,
+		$par_dt_nasc, $par_nome_pai, $par_nome_mae,
+		$par_doc_rg, $par_doc_cpf) {
+		
+		}
+	}
+
+	class usuario{
+
+	}
+	class cliente {
+
+	}
+?>
